b359c33e982f0ada6ea3fe18dd913ff834b991fb
Robin Martijn <ik@robinmartijn.nl>
2022-04-27 23:31:50 +0200

    ✨ set up multiple platforms

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,34 @@ use Spatie\Browsershot\Browsershot;
 |
 */
 
-Route::get('/{url}', function ($url) {
-    if (! file_exists(public_path('screenshots/' . $url . '.png'))) {
+Route::get('/{platform}/{url}', function ($platform, $url) {
+    if (! in_array($platform, ['twitter', 'facebook'])) $platform = 'unknown';
+
+    $preferred_size = [
+        'twitter' => [
+            'width' => '600',
+            'height' => '600',
+        ],
+        'facebook' => [
+            'width' => '1200',
+            'height' => '630',
+        ],
+        'unknown' => [
+            'width' => '1200',
+            'height' => '630',
+        ],
+    ];
+
+    if (! file_exists(public_path('screenshots/' . $platform . '/' . $url . '.png'))) {
         $decoded_url = base64_decode($url);
 
         Browsershot::url($decoded_url)
-            ->windowSize(1200, 630)
-            ->save('screenshots/' . $url . '.png');
+            ->windowSize($preferred_size[$platform]['width'], $preferred_size[$platform]['height'])
+            ->save('screenshots/' . $platform . '/' . $url . '.png');
     }
 
     // Get the contents of the screenshot
-    $contents = file_get_contents(asset('screenshots/' . $url . '.png'));
+    $contents = file_get_contents(asset('screenshots/' . $platform . '/' . $url . '.png'));
 
     // Return the image
     return response($contents, 200)->header('Content-Type', 'image/png');
